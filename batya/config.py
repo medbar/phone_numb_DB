737c6006d33d96c7ce810599082eb67cd6f3087a
Anton <tonymit@yandex.ru>
2017-04-11 20:37:23 +0300

    release

--- batya/config.py
+++ batya/config.py
@@ -1,8 +1,8 @@
 
 dbconnet = {
-            "user": "phonedownloader",
-            "password": "R!f<fkmvjyn",
-            "host": "192.168.1.106",
+            "user": "",
+            "password": "",
+            "host": "",
             "database": "db_russian_numbering_plan",
             "use_unicode": True,
             "charset": 'utf8'
